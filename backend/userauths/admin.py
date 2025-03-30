d1ca74bbfc7983fd09dff790cb3db0ac352a2e3f
Sandip Das <sandip4991@gmail.com>
2025-03-30 21:00:03 +0530

    feat: Create User and Profile Model

--- backend/userauths/admin.py
+++ backend/userauths/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import User, Profile
 
-# Register your models here.
+admin.site.register(User)
+
+admin.site.register(Profile)
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'image', 'full_name', 'country', 'date']
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'email', 'full_name', 'otp']
